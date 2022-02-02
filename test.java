4fa84a1cb63bf7e0c743978f165ad9298fe2db9d
chunkithui <95217215+chunkithui@users.noreply.github.com>
2022-02-02 18:47:55 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package warmupsiebelom;
+
+import java.util.List;
+import java.io.*;
+import java.net.*;
+import javax.net.ssl.*;
+import java.security.cert.X509Certificate;
