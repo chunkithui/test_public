209c29cbc679ea61b912a86ad1f064544a42b0f8
chunkithui <95217215+chunkithui@users.noreply.github.com>
2022-02-03 23:21:20 +0800

    Update test.java

--- test.java
+++ test.java
@@ -13,7 +13,6 @@ import java.security.cert.X509Certificate;
 
 import java.security.MessageDigest;
 import java.util.Arrays;
-import java.util.Properties;
 
 public static void main(String[] args) throws Exception {
 }
